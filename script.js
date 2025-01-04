f415b1af1ceda6b1c4a21f23c4eb1f0c36e3b88e
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-04 11:06:50 -0800

    allow user to create lines/shapes on client

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ mainContainer.style.alignItems = "center";
 
 //Content Container
 const container = document.querySelector(".content");
-container.style.border = "solid 1px red";
+container.style.border = "solid 20px red";
+container.style.borderRadius = "20px"
 container.style.width = "640px";
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
@@ -20,14 +21,47 @@ container.style.flexWrap = "wrap";
 const createGrid = () => {
     for (i = 0; i < 256; i++){
         const square = document.createElement("div");
+        square.classList = "block";
         square.style.height = "40px";
         square.style.boxSizing = "border-box";
         square.style.aspectRatio = "1/1";
         square.style.backgroundColor = "lightgrey";
         square.style.display = "flex";
         square.style.border = "dotted 1px black";
+
         container.appendChild(square);
-    }
+        
+        //On Hover Color Styling
+        square.addEventListener('mouseover', e => {
+            e.target.style.backgroundColor = "lightblue";
+        })
+
+        square.addEventListener('mouseout', e =>{
+            e.target.style.backgroundColor = "lightgrey";
+        })
+
+
+
+    };
 };
 createGrid();
+
+//Mouse Hovering Styling
+const block = document.querySelectorAll('.block');
+block.forEach(block => {
+    block.addEventListener('mouseover', e=> {
+        block.addEventListener('mousemove', e => {
+        const dot = document.createElement('div');
+        dot.style.position = "absolute";
+        dot.style.top = `${e.clientY}px`;
+        console.log("Client Y: ", e.clientY);
+        dot.style.left = `${e.clientX}px`;
+        console.log("Client X: ", e.clientY);
+        dot.style.backgroundColor = "red";
+        dot.style.width = "1px";
+        dot.style.height = "1px";
+        block.appendChild(dot);
+        });
+    });
+})
 
